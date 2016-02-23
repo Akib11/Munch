8222e96807076526a7df8d0f32d9824847885952
Akib11 <Akibm92@gmail.com>
2016-02-23 12:37:42 +0000

    Initial Commit

---
+++ Munch/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Munch
+//
+//  Created by Akib Quraishi on 23/02/2016.
+//  Copyright © 2016 AkibiOS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
